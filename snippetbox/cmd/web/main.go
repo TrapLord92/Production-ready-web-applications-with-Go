a9b5c3c3298d42d1c016eecc7e44915381ea12e4
TrapLord92 <traplord345@gmail.com>
2024-09-30 04:15:35 +0500

    teory more

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -19,7 +19,7 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Println("Starting server on :4000")
+	log.Println("Starting server on port :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
