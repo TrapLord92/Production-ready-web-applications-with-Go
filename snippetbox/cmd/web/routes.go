8685f569c4177ca5328988e2a18474e120493ee5
TrapLord92 <traplord345@gmail.com>
2024-10-01 20:35:31 +0500

    Panic recovery

--- snippetbox/cmd/web/routes.go
+++ snippetbox/cmd/web/routes.go
@@ -2,8 +2,6 @@ package main
 
 import "net/http"
 
-// Update the signature for the routes() method so that it returns a
-// http.Handler instead of *http.ServeMux.
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -11,8 +9,6 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	// Pass the servemux as the 'next' parameter to the secureHeaders middleware.
-	// Because secureHeaders is just a function, and the function returns a
-	// http.Handler we don't need to do anything else.
-	return secureHeaders(mux)
+	// Wrap the existing chain with the recoverPanic middleware.
+	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
 }
