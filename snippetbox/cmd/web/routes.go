78139d3447c33dd00621f221f641d8ad4a2542aa
TrapLord92 <traplord345@gmail.com>
2024-10-01 21:21:17 +0500

    Clean URLs and method-based

--- snippetbox/cmd/web/routes.go
+++ snippetbox/cmd/web/routes.go
@@ -3,19 +3,24 @@ package main
 import (
 	"net/http"
 
-	"github.com/justinas/alice" // New import
+	"github.com/julienschmidt/httprouter" // New import
+	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
+	// Initialize the router.
+	router := httprouter.New()
+	// Update the pattern for the route for the static files.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	// Create a middleware chain containing our 'standard' middleware
-	// which will be used for every request our application receives.
+	router.Handler(http.MethodGet, "/static/*filepath",
+		http.StripPrefix("/static", fileServer))
+	// And then create the routes using the appropriate methods, patterns and
+	// handlers.router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
+	// Create the middleware chain as normal.
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-	// Return the 'standard' middleware chain followed by the servemux.
-	return standard.Then(mux)
+	// Wrap the router with the middleware and return it as normal.
+	return standard.Then(router)
 }
